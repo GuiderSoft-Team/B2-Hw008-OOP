a7ffd008a585d5fda2e100d8f099f47a7c57dec9
YusufAmat <hoccayusuf@gmail.com>
2022-11-29 11:25:48 +0100

    B2-Hw008-OOP ist erledigt.

--- src/MainApp.java
+++ src/MainApp.java
@@ -4,11 +4,11 @@ public class MainApp {
 
         Point p1 = new Point(1, 2);
         Point p2 = new Point(4, 6);
-        double uzaklik1 = Point.calculateDistance(p1, p2);
+        double uzaklik1 = p1.calculateDistance(p2);
         System.out.printf("%s ve %s noktalari arasindaki uzaklik = %5.2f\n", p1, p2, uzaklik1);
 
         Point p3 = new Point(3, 6);
-        double uzaklik2 = Point.calculateDistance(p3.getX(), p3.getY(), 5, 4);
+        double uzaklik2 = p3.calculateDistance(5,4);
         System.out.printf("%s ve Point (5;4) noktalari arasindaki uzaklik = %5.2f\n", p3, uzaklik2);
     }
 }
--- src/Point.java
+++ src/Point.java
@@ -32,13 +32,13 @@ public class Point {
         return "Point (" + x + ";" + y + ")";
     }
 
-    public static double calculateDistance(Point p1, Point p2) {
-        return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+    public double calculateDistance(Point p) {
+        return Math.sqrt((this.x - p.x) * (this.x - p.x) + (this.y - p.y) * (this.y - p.y));
     }
 
-    public static double calculateDistance(int x1, int y1, int x2, int y2) {
-        x1 -= x2;
-        y1 -= y2;
-        return Math.sqrt(x1 * x1 + y1 * y1);
+    public double calculateDistance(int x, int y) {
+        x -=this.x;
+        y -= this.y;
+        return Math.sqrt(x * x + y * y);
     }
 }
