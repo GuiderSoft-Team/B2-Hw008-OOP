f5426ad1326533031b2f1658cea8c651b5402e53
GİZEM YAYLAGÜL <gizemyaylagul@gmail.com>
2022-11-29 20:48:53 +0300

    Gizem Yaylagül Homework8

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,6 +1,10 @@
 public class MainApp {
     public static void main(String[] args) {
-        //readme.me dosyasını okuyunuz.
+      Point p1= new Point(9,3);
+      Point p2= new Point(5,4);
+      Point p3= new Point(6,1);
 
+        System.out.println("p1 ve p2 arasındaki mesafe "+p1.calculateDistance(p2));
+        System.out.println("p3 ile 5 ve 4 değerlerinin mesafesi "+p3.calculateDistance(5,4));
     }
 }
