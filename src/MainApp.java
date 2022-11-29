55a50facfd5549a30404133cfc76e1b5091ec85b
YusufAmat <hoccayusuf@gmail.com>
2022-11-29 09:52:13 +0100

    B2-Hw008-OOP ist erledigt.

--- src/MainApp.java
+++ src/MainApp.java
@@ -2,5 +2,13 @@ public class MainApp {
     public static void main(String[] args) {
         //readme.me dosyasını okuyunuz.
 
+        Point p1 = new Point(1, 2);
+        Point p2 = new Point(4, 6);
+        double uzaklik1 = Point.calculateDistance(p1, p2);
+        System.out.printf("%s ve %s noktalari arasindaki uzaklik = %5.2f\n", p1, p2, uzaklik1);
+
+        Point p3 = new Point(3, 6);
+        double uzaklik2 = Point.calculateDistance(p3.getX(), p3.getY(), 5, 4);
+        System.out.printf("%s ve Point (5;4) noktalari arasindaki uzaklik = %5.2f\n", p3, uzaklik2);
     }
 }
---
+++ src/Point.java
@@ -0,0 +1,44 @@
+public class Point {
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point() {
+        this(0, 0);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "Point (" + x + ";" + y + ")";
+    }
+
+    public static double calculateDistance(Point p1, Point p2) {
+        return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+    }
+
+    public static double calculateDistance(int x1, int y1, int x2, int y2) {
+        x1 -= x2;
+        y1 -= y2;
+        return Math.sqrt(x1 * x1 + y1 * y1);
+    }
+}
