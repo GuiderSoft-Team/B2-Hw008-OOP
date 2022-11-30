d9e8f02b0127ad501faaf88dafa2fe66d36d244c
ademgencer <ademyasargencer@gmail.com>
2022-12-01 00:03:09 +0300

    Adem Gencer - Homework 8 -

--- src/MainApp.java
+++ src/MainApp.java
@@ -6,14 +6,17 @@ public class MainApp {
         Point p2 = new Point(8, 16);
 
         Point p3 = new Point();
+
         p3.setX(12);
+
         p3.setY(28);
+
 
         System.out.println("p3 noktası : " + p3);
 
-        System.out.printf("p3 ile (5,4) noktaları arası mesafe: %5.2f\n", p3.calculateDistance(p3, 5, 4));
+        System.out.printf("p3 ile (5,4) noktaları arası mesafe: %5.2f\n", p3.calculateDistance(5, 4));
 
-        System.out.printf("p1 ve p2 noktaları arası mesafe: %5.2f\n", p1.calculateDistance(p1, p2));
+        System.out.printf("p1 ve p2 noktaları arası mesafe: %5.2f\n", p1.calculateDistance(p2));
 
     }
 }
