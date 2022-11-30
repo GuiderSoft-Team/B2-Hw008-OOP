9e32c41f5dec1ea288f21e41e88aa390a6158ebd
yasinmurat <muratyeni671@gmail.com>
2022-11-30 14:18:29 +0300

    Yasin Murat Homework8

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,6 +1,14 @@
 public class MainApp {
     public static void main(String[] args) {
         //readme.me dosyasını okuyunuz.
+        Point p1=new Point(5,7);
+        Point p2=new Point(2,4);
+        Point p3=new Point(6,5);
+
+
+        System.out.printf("p1 ve p2 noktaları arası uzaklık =%5.2f ", p1.calculateDintance(p2));
+        System.out.println();
+        System.out.printf("p3 noktasının  (5;4)  'e uzaklığı  :%5.2f ", p3.calculateDintance(5,4));
 
     }
 }
